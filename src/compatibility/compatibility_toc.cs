7a100c9c07877ee70c5fbdf5dff8795a5a5525a7
Guru Nagarajan <gurunagarajan@google.com>
2013-07-23 15:13:01 -0700

    JB-MR2 updates to CDD 	new file:   4.3/android-4.3-cdd.pdf 	new file:   4.3/versions.jd 	new file:   android-4.3-cdd.pdf 	modified:   compatibility_toc.cs 	modified:   cts-development.jd 	modified:   downloads.jd
    Change-Id: I8551150c64720b4c6c34fafcff9c30cab7c01e43

--- src/compatibility/compatibility_toc.cs
+++ src/compatibility/compatibility_toc.cs
@@ -26,7 +26,7 @@
       <li><a href="<?cs var:toroot ?>compatibility/overview.html">CTS Overview</a></li>
       <li><a href="<?cs var:toroot ?>compatibility/cts-intro.html">CTS Introduction</a></li>
       <li><a href="<?cs var:toroot ?>compatibility/cts-development.html">CTS Development</a></li>
-      <li><a href="<?cs var:toroot ?>compatibility/android-4.2-cdd.pdf">Compatibility Definition Document (CDD)</a></li>
+      <li><a href="<?cs var:toroot ?>compatibility/android-4.3-cdd.pdf">Compatibility Definition Document (CDD)</a></li>
       <li><a href="<?cs var:toroot ?>compatibility/downloads.html">Downloads</a></li>
       <li><a href="<?cs var:toroot ?>compatibility/contact-us.html">Contact Us</a></li>
     </ul>
