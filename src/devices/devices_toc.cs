1d734fae3a171c148044c609014718f718a72730
Clay Murphy <claym@google.com>
2015-09-02 14:59:08 -0700

    Docs: Move FS config to new Configuration section
    Bug: 20102980
    Change-Id: I12462f41352846662794fd8107fa81b983f9aabc

--- src/devices/devices_toc.cs
+++ src/devices/devices_toc.cs
@@ -192,7 +192,7 @@
             </a>
         </div>
         <ul>
-          <li><a href="<?cs var:toroot ?>devices/tech/filesystem-config.html">File System Configuration</a></li>
+          <li><a href="<?cs var:toroot ?>devices/tech/config/filesystem.html">File System</a></li>
           <li><a href="<?cs var:toroot ?>devices/tech/config/kernel.html">Kernel</a></li>
           <li><a href="<?cs var:toroot ?>devices/tech/config/low-ram.html">Low RAM</a></li>
           <li><a href="<?cs var:toroot ?>devices/tech/config/renderer.html">OpenGLRenderer</a></li>
