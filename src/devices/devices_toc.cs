4727603e65c6fd80b25eadc0096acc512be35700
Bert McMeen <amcmeen@google.com>
2015-09-11 14:57:27 -0700

    Docs: Addition of Authentication page
    Bug: 21114452
    Change-Id: I2d652b9453fcf87d1582d5b3a71296fe3970822b

--- src/devices/devices_toc.cs
+++ src/devices/devices_toc.cs
@@ -308,6 +308,7 @@
               </a>
             </div>
             <ul>
+              <li><a href="<?cs var:toroot ?>devices/tech/security/authentication/index.html">Authentication</a></li>
               <li><a href="<?cs var:toroot ?>devices/tech/security/encryption/index.html">Encryption</a></li>
               <li class="nav-section">
                 <div class="nav-section-header">
